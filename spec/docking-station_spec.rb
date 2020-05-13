c461b6cea535524597cddfc006fd4a5b8fdeaa67
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-05-13 17:28:50 +0100

    add instance of bike

--- spec/docking-station_spec.rb
+++ spec/docking-station_spec.rb
@@ -1,8 +1,17 @@
 require 'docking-station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+it 'returns docked bikes' do
+  bike = Bike.new
+  subject.dock(bike)
+  expect(subject.bike).to eq bike
 end
-
-# it 'responds to release_bike' do
-#   expect(subject).to respond_to :release_bike
-# end
+end
